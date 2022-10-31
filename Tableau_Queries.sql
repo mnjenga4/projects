b97f7166d37e6f89030d1feb0c3256c54cf3b325
mnjenga4 <115945824+mnjenga4@users.noreply.github.com>
2022-10-31 11:13:29 -0500

    Add files via upload

---
+++ Tableau_Queries.sql
@@ -0,0 +1,30 @@
+-- Queries for Tableau visualization
+
+--(1.)
+
+
+SELECT SUM(new_cases) AS total_cases, SUM(new_deaths) AS total_deaths, (SUM(new_deaths)/SUM(new_cases))*100 AS DeathPercentage 
+FROM dbo.Covid_19_Deaths
+WHERE continent IS NOT NULL
+--GROUP BY [date]
+ORDER BY 1,2
+
+--(2.)
+SELECT [location], SUM(CAST(new_deaths AS int)) AS TotalDeathCount
+FROM dbo.Covid_19_Deaths
+WHERE continent IS NULL
+AND location NOT IN ('European Union', 'High income', 'World', 'Upper middle income', 'Low income', 'Lower middle income', 'International')
+GROUP BY [location]
+ORDER BY TotalDeathCount DESC
+
+--(3.)
+SELECT location, population, MAX(total_cases) AS HighestInfectionCount, MAX(total_cases / population)*100 AS PercentPopulationInfected
+FROM dbo.Covid_19_Deaths
+GROUP BY location, population
+ORDER BY PercentPopulationInfected DESC
+
+--(4.)
+SELECT location, population, date, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases / population))*100 AS PercentPopulationInfected
+FROM dbo.Covid_19_Deaths
+GROUP BY location, population, date
+ORDER BY PercentPopulationInfected DESC
